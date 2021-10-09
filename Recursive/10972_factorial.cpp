6ca3f74cc3f67878227fd6dcfd8afa764379e4e1
toysin <toysin1203@hotmail.com>
2021-10-09 23:34:18 +0900

    Create 10972_factorial.cpp

---
+++ Recursive/10972_factorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int	factorial(int n);
+
+int	main()
+{
+	int	n;
+
+	std::cin >> n;
+	std::cout << factorial(n);
+
+	return 0;
+}
+
+int	factorial(int n)
+{
+	if (n < 2)
+		return 1;
+	return (n * factorial(n - 1));
+}
