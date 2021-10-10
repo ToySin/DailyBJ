a8c7784994335429e84f51f45768c330c8f56041
toysin <toysin1203@hotmail.com>
2021-10-10 17:19:52 +0900

    Update 1929_print_prime.cpp
    using "printf"

--- Math/1929_print_prime.cpp
+++ Math/1929_print_prime.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
 
 
-int	isPrime(int n)
+void	Eratos(bool *arr, int n)
 {
-	int	i = 2;
-
-	if (n < 2)
-		return 0;
-	while (i * i <= n)
+	arr[1] = false;
+	for (int i = 2; i * i <= n; i++)
 	{
-		if (n % i == 0)
-			return 0;
-		i++;
+		if (arr[i])
+			for (int j = i * i; j <= n; j += i)
+				arr[j] = false;
 	}
-	return 1;
 }
 
 int	main()
 {
 	int	m, n;
+	bool	*arr;
 
 	std::cin >> m >> n;
-	while (m <= n)
-	{
-		if (isPrime(m))
-			std::cout << m << std::endl;
-		m++;
-	}
+	arr = new bool[n + 1];
+	for (int i = 0; i <= n; i++)
+		arr[i] = true;
+	Eratos(arr, n);
+	for (int i = m; i <= n; i++)
+		if (arr[i])
+			printf("%d\n", i);
+	delete[] arr;
 	return 0;
 }
