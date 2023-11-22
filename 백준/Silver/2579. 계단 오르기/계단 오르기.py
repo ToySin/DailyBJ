3c799d12e02f05bd6cd0db886ebf21263d4110e9
신동빈 <40738437+ToySin@users.noreply.github.com>
2023-11-22 18:10:45 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,3 +1,33 @@
+# 최종 목표는 마지막 도착 계단에 최고 점수와 함께 도달하는 것
+# 매 계단에 최고 점수로 도달하는 법을 찾자
+
+# 계단은 한 계단 or 두 계단 씩 오를 수 있음
+# 연속으로 세개의 계단을 밟을 순 없음
+# 일단 내 계단까지 오는걸 생각해보면
+# 두칸 전에서 올라오냐? 한칸 전에서 올라오냐? 로 나뉜다.
+# 밑에서 잘 했겠거니와.. 하면서 이번칸에서 두 방법중 최댓값을 구한다.
+
+# 최저 계단에 도착했다 -> step == 0
+# def dfs(step, cnt):
+#     # 시작점
+#     if step < 0:
+#         return 0
+#     # 최저 계단
+#     if step == 0:
+#         dp[step] = arr[step]
+#     # 이미 결과를 아는 경우
+#     if dp[step] != 0:
+#         return dp[step]
+
+#     # 연속으로 1개째 밟고 있는 경우
+#     if cnt == 1:
+#         dp[step] = arr[step] + max(dfs(step-1, cnt+1), dfs(step-2, 1))
+#     # 연속으로 2개째 밝고 있는 경우 무조건 두칸
+#     else:
+#         dp[step] = arr[step] + dfs(step-2, 1)
+#     return dp[step]
+
+
 N = int(input())
 arr = [int(input()) for _ in range(N)]
 
@@ -19,3 +49,6 @@ for i in range(3, N):
     dp[i] = max(dp[i-3] + arr[i-1] + arr[i], dp[i-2] + arr[i])
 
 print(dp[-1])
+
+# 올바르게 올라오게 만들지 말고
+# 올바르게 올라온 상황이 어떤 상황인지 생각하기
